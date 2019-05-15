e2ff9fc263ae65b39a20afcc28682fc0077b40bf
Deniz <deniz.lektemur@student.hu.nl>
2019-05-15 12:38:56 +0200

    Test claw with potentiometer feedback and pwm lib

--- code/main.cpp
+++ code/main.cpp
@@ -1,26 +1,42 @@
 #include "hwlib.hpp"
+#include <pwm.hpp>
 
 int main(void) {
     // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
+ 
+    auto pwm_0 = R2D2::pwm_lib::pwm_c(0);
+    auto pot = hwlib::target::pin_adc(hwlib::target::ad_pins::a0);
 
-    auto pwm = hwlib::target::pin_out(hwlib::target::pins::d9);
-    auto pot = hwlib::target::pin_adc(hwlib::target::ad_pins::a2);
-    pwm.write(0);
-    for (;;) {
-        pwm.write(1);
-        hwlib::wait_us(2400);
-        pwm.write(0);
-        hwlib::wait_ms(1000);
+    int stand = 0;
+    bool second = false;
+    bool done = false;
 
-        hwlib::cout << "wait 2400 " << pot.read() << '\n';
+    for(;;) {
+        if(!done) {
+            int potvalue = 0;
 
-        pwm.write(1);
-        hwlib::wait_us(1000);
-        pwm.write(0);
-        hwlib::wait_ms(1000);
-
-        hwlib::cout << "wait 1000 " << pot.read() << '\n';
+            int calc = 132 * stand + 1250;
+            for(int i = 0; i < 10; i++) {
+                potvalue += pot.read();
+                hwlib::wait_ms(10);
+            }
+            potvalue /= 10;
+            if (potvalue < (calc-100) || potvalue > (calc+100)){
+                hwlib::cout << "ik heb iets vast\n";
+                stand--;
+                if(second){
+                    done = true;
+                }
+                second = true;
+            }
+            else {
+                stand++;
+                hwlib::cout << "ik ga verder\n";
+            }
+        }
+        pwm_0.change_duty_cycle(stand+13);
+        hwlib::wait_ms(500);
     }
 }
