1279e8fc13cd2f437d92181bd39cc784dd0a213a
Deniz <deniz.lektemur@student.hu.nl>
2019-05-21 14:35:29 +0200

    Split the close function into two functions

--- code/headers/claw.hpp
+++ code/headers/claw.hpp
@@ -13,6 +13,7 @@ namespace r2d2::end_effectors {
         void close();
         virtual void reset() override;
     private:
+        int32_t calc_pot_difference();
         hwlib::target::pin_adc pot;
         R2D2::pwm_lib::pwm_c pwm;
         const uint16_t pot_offset = 1250;
--- code/src/claw.cpp
+++ code/src/claw.cpp
@@ -11,26 +11,31 @@ namespace r2d2::end_effectors {
         pwm.change_duty_cycle(open_pwm);
     }
     
-    void claw_c::close() {
+    int32_t claw_c::calc_pot_difference() {
         uint32_t pot_value;
         uint32_t expected_pot;
         int32_t difference;
+        pot_value = 0;
+        expected_pot = pot_per_pwm_step * (i-open_pwm) + pot_offset;
+        for(uint8_t j = 0; j < pot_scans; j++) {
+            pot_value += pot.read();
+            hwlib::wait_ms(1);
+        }
+        pot_value /= pot_scans;
+        difference = pot_value - expected_pot;
+        return difference;
+    }
+
+    void claw_c::close() {
         bool collision = false;
+        int32_t difference;
         open();
         hwlib::wait_ms(1000);
         for(uint8_t i = open_pwm; i <= closed_pwm; i++) {
             if(!collision) {
                 pwm.change_duty_cycle(i);
                 hwlib::wait_ms(20);
-                pot_value = 0;
-                expected_pot = pot_per_pwm_step * (i-open_pwm) + pot_offset;
-                for(uint8_t j = 0; j < pot_scans; j++) {
-                    pot_value += pot.read();
-                    hwlib::wait_ms(1);
-                }
-                pot_value /= pot_scans;
-                difference = pot_value - expected_pot;
-                hwlib::cout << pot_value << "  " << difference << '\n';
+                difference = calc_pot_difference();
                 if(difference > 100 || difference < -100) {
                     collision = true;
                 }
