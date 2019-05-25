8b94dddca2dc0ff310085597918ebe0639bc7055
victo <victor.adamse@student.hu.nl>
2019-05-25 12:53:52 +0200

    worked on process function

--- code/src/module.cpp
+++ code/src/module.cpp
@@ -1,11 +1,24 @@
 #include <module.hpp>
-    
+#include <claw.hpp>
+
 namespace r2d2::end_effectors {
     module_c::module_c(base_comm_c &comm, end_effector_c *end_effector) 
         : base_module_c(comm), end_effector(end_effector) {
-            comm.listen_for_frames(
-                { frame_type::ALL }
-            );
+            if(end_effector == nullptr){
+                return;
+            }
+            switch(end_effector->get_type()){
+                case end_effector_type::CLAW:
+                    comm.listen_for_frames(
+                        { frame_type::END_EFFECTOR_CLAW }
+                    );
+                break;
+                default:
+                    comm.listen_for_frames(
+                        { frame_type::NONE }
+                    );
+                break;
+            }
         }
 
     void module_c::process() {
@@ -13,8 +26,29 @@ namespace r2d2::end_effectors {
             auto frame = comm.get_data();
             //process the frame
 
-            if (end_effector != nullptr) {
+            if(frame.request) {
+                continue;
+            }
 
+            if (end_effector == nullptr) {
+                continue;
+            }
+
+            switch(end_effector->get_type()){
+                case end_effector_type::CLAW:
+                    auto claw = dynamic_cast<claw_c*>(end_effector);
+                    const auto close = frame.as_frame_type<
+                        frame_type::END_EFFECTOR_CLAW
+                    >().close;
+                    if(close){
+                        claw->close();
+                    } else {
+                        claw->open();
+                    }
+                break;
+                default:
+                
+                break;
             }
         }
     }
