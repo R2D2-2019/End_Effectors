b2ce2d6bc436be5ec25d085d8b1e1225a7c46c27
victo <victor.adamse@student.hu.nl>
2019-05-28 15:36:42 +0200

    removed enum (enum is now in internal comm )

--- code/headers/end_effector.hpp
+++ code/headers/end_effector.hpp
@@ -5,13 +5,6 @@
 namespace r2d2::end_effectors {
 
     /**
-     * This enum contains the different end effector types.
-     */
-    enum end_effector_type {
-        CLAW
-    };
-
-    /**
      * Class end_effector_c is the abstract base class for all end effectors.
      * It is used by the interface as a listener
      */
