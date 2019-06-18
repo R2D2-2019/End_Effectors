b677774965fc6fae2e8b1f38f48a3302d489dc2c
Deniz <deniz.lektemur@student.hu.nl>
2019-06-18 16:17:15 +0200

    replaced new with a pointer

--- code/main.cpp
+++ code/main.cpp
@@ -12,9 +12,12 @@ int main(void) {
 
     r2d2::comm_c comm;
 
-    auto end_effector = new r2d2::end_effectors::claw_c();
+    auto end_effector = r2d2::end_effectors::claw_c();
+    r2d2::end_effectors::claw_c *claw;
+    claw = &end_effector;
 
-    auto module = r2d2::end_effectors::module_c(comm, end_effector);
+
+    auto module = r2d2::end_effectors::module_c(comm, claw);
 
     for(;;) {
         module.process();
