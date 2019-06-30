8d72b11f3acb8f2f352158eca13de4cccf04e9c1
Deniz <deniz.lektemur@student.hu.nl>
2019-06-30 12:08:58 +0200

    formatted files

--- code/headers/module.hpp
+++ code/headers/module.hpp
@@ -13,20 +13,21 @@ namespace r2d2::end_effectors {
     class module_c : public base_module_c {
     private:
         end_effector_c &end_effector;
+
     public:
         /**
          * The constructor initializes:
          * base_module_c
          * the pointer to the end_effector
-         * 
-         * @param comm 
+         *
+         * @param comm
          * @param end_effector
          */
         module_c(base_comm_c &comm, end_effector_c &end_effector);
-    
+
         /**
          * Let the module process data.
-         */ 
+         */
         void process() override;
     };
-}
+} // namespace r2d2::end_effectors
